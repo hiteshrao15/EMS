4513dc80774696d4d087255b7d776d63cb0b1455
Hitesh <143160062+hiteshrao15@users.noreply.github.com>
2026-06-25 10:52:49 +0530

    .

--- api/[[...all]].js
+++ api/[[...all]].js
@@ -1,7 +1,8 @@
 const app = require("../server");
 
 // Vercel sends requests to /api/... so we strip the /api prefix
-// before passing to Express, where routes are mounted at /employees
+// before passing to Express, where routes are mounted at /employees.
+// Using [[...all]].js as an optional catch-all catches all /api/* routes.
 module.exports = (req, res) => {
   req.url = req.url.replace(/^\/api/, "") || "/";
   return app(req, res);
